b6fe8fbd327d2ad9c376d1590754fa194c9e9604
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-10-12 23:03:34 +0530

    feat(condtional statement )

---
+++ _06_conditional_statement/app.js
@@ -0,0 +1,65 @@
+
+//  conditional statement 
+
+// if  -> executed only when condition is true
+// else ->  block of code execute when if condition false  
+// else if  
+
+const travelOption ="";
+
+if(travelOption=="car"){
+  console.log(" i have to go  via car ")
+}
+else if (travelOption=="bike"){
+  console.log("go via bike ")
+}else{
+  console.log("go via onside road ");
+}
+
+
+
+
+
+
+
+
+
+
+// switch case 
+
+// 1. 
+// 2<0  false -> if body :block of code not execute in this case
+if(2<0){
+  console.log("if statement ....."); // 
+}
+
+// real application 
+var isLogin = true;
+if(isLogin){
+  console.log("redirect to dashboard page .../")
+}else {
+    console.log("redirect login page...")
+}
+
+
+
+
+// block
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//1️⃣ Problem: Check if a person is eligible to vote
+//2️⃣ Problem: Check whether a number is even or odd
+//3️⃣ Problem: Grade a student based on marks
+
